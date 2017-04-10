d83c27258a834655a97f23fcbad606a824c4c0b8
Naren <naren@narendasan.com>
2017-04-10 13:27:36 -0700

    add go response

--- heartbeats/heartbeat.go
+++ heartbeats/heartbeat.go
@@ -7,3 +7,15 @@
 * University of Illinois/NCSA Open Source License. You should have
 * received a copy of this license in a file with the distribution.
 **/
+
+//Add a route for arbor 
+services.Route {
+	"Heartbeat",
+	"GET",
+	"/heartbeat",
+	Heartbeat 
+}
+
+func Heartbeat(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "GROOT");
+}
