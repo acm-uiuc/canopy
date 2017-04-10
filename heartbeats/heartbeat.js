db0e5225637c19f17a99bf925124c3634623c6e4
Naren <naren@narendasan.com>
2017-04-10 13:36:45 -0700

    add js response

--- heartbeats/heartbeat.js
+++ heartbeats/heartbeat.js
@@ -7,3 +7,7 @@
 * University of Illinois/NCSA Open Source License. You should have
 * received a copy of this license in a file with the distribution.
 **/
+
+app.get("/heartbeat", function(req, res){
+  return res.json("GROOT");
+});
