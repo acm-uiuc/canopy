5e7b9c1e732c9f6cec558f2ceaaed6d841a06cbb
Naren Dasan <naren@narendasan.com>
2017-04-09 22:49:04 -0700

    Heartbeat Class

--- src/monitor/heartbeat/heartbeat.rs
+++ src/monitor/heartbeat/heartbeat.rs
@@ -12,13 +12,23 @@ extern crate hyper;
 use hyper::*;
 use std::io::Read;
 
-fn main() {
-    let client = Client::new();
-    let res = client.post("http://localhost:3000/set").body(r#"{ "msg": "Just trust the Rust" }"#).send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-    let mut res = client.get("http://localhost:3000/").send().unwrap();
-    assert_eq!(res.status, hyper::Ok);
-    let mut s = String::new();
-    res.read_to_string(&mut s).unwrap();
-    println!("{}", s);
+struct Heart {
+    String Applications;
+}
+
+impl Heartbeat {
+   pub fn Beat() {
+       let client = Client::new();
+       let res = client.post("http://localhost:3000/set").body(r#"{ "msg": "Just trust the Rust" }"#).send().unwrap();
+       assert_eq!(res.status, hyper::Ok);
+       let mut res = client.get("http://localhost:3000/").send().unwrap();
+       assert_eq!(res.status, hyper::Ok);
+       let mut s = String::new();
+       res.read_to_string(&mut s).unwrap();
+       println!("{}", s); 
+   }
+
+   pub fn new(String: Apps) -> self {
+       Heart {Applications: Apps}
+   }
 }
