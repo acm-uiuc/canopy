cbd32050d5ed76a40b8337305ac3b4f0ae505fca
Naren <naren@narendasan.com>
2017-04-10 13:37:38 -0700

    add rb response

--- heartbeats/heartbeat.rb
+++ heartbeats/heartbeat.rb
@@ -5,3 +5,7 @@
 # The Groot Project is open source software, released under the 
 # University of Illinois/NCSA Open Source License. You should have
 # received a copy of this license in a file with the distribution.
+
+get '/status' do
+    ResponseFormat.message("GROOT")
+end
