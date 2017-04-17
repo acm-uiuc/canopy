41d48cda4147c5c547298688cef29c396672ccc0
Naren Dasan <naren@narendasan.com>
2017-04-17 12:11:17 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@
 // University of Illinois/NCSA Open Source License. You should have
 // received a copy of this license in a file with the distribution.
 
-mod canopy_server;
+mod server;
 
 fn main() {
-    canopy::canopy_server();
+    server::canopy_server();
 }
