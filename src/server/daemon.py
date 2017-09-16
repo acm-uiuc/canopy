c8302cc3bdc9036d41233f3b5b06c300ca310f5a
Tanishq Dubey <tanishq.dubey@gmail.com>
2017-09-16 11:54:20 -0500

    New canopy infra

---
+++ src/server/daemon.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import sys, os, time, atexit
+from signal import SIGTERM
+
+class Daemon:
+    def __init__(self, pidfile, stdin='/dev/null', stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def daemonize(self):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("Fork 1 failed!\n%d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        os.chdir("/")
+        os.setsid()
+        os.unmask(0)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError, e:
+            sys.stderr.write("Fork 2 failed!\n%d (%s)\n" % (e.errno, e.strerror))
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = file(self.stdin, 'r')
+        so = file(self.stdout, 'a+')
+        se = file(self.stderr, 'a+', 0)
+
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        file(self.pidfile, 'w+').write("%s\n" % pid)
+
+        def delpid(self):
+            os.remove(self.pidfile)
+
+        def start(self):
+            try:
+                pf = file(self.pidfile, 'r')
+                pid = int(pf.read().strip())
+                pf.close()
+            except IOError:
+                pid = None
+
+            if pid:
+                message = "PID File %s already exists. Daemon might be already running?\n"
+                sys.stderr.write(message % self.pidfile)
+                sys.exit(1)
+
+            self.daemonize()
+            self.run()
+
+        def stop(self):
+            try:
+                pf = file(self.pidfile, 'r')
+                pid = int(pf.read().strip())
+                pf.close()
+            except IOError:
+                pid = None
+
+            if not pid:
+                message = "PID File %s does not exist. Daemon might not be running?\n"
+                sys.stderr.write(message % self.pidfile)
+                return
+
+            try:
+                while 1:
+                    os.kill(pid, SIGTERM)
+                    time.sleep(0.1)
+            except OSError, err:
+                err = str(err)
+                if err.find("No such process") > 0:
+                    if os.path.exists(self.pidfile):
+                        os.remove(self.pidfile)
+                else:
+                    print str(err)
+                    sys.exit(1)
+
+        def restart(self):
+            self.stop()
+            self.start()
+
+        def run(self):
+            pass
